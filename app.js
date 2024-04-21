3f56cb65e30ee4b9c90ef3dea8dd4b93e254d7df
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-21 23:58:08 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // write code here
+        let a = undefined;
+        let b = ' ';
+        let c = !b;
+
+        
+        let d = Boolean(a);
+        let e = Boolean(b);
+        let f = Boolean(c);
+
+        console.log(d); 
+        console.log(e); 
+        console.log(f); 
+    ผลลัพธ์
+false
+true
+false
+d = false: เนื่องจากตัวแปร a มีค่าเป็น undefined ซึ่งถือว่าเป็น false value
+e = true: เนื่องจากตัวแปร b มีค่าเป็นช่องว่าง (space) ซึ่งไม่ถือว่าเป็น false value
+f = false: เนื่องจากตัวแปร c ถูกนิเสธ (!b) จะกลายเป็น false
